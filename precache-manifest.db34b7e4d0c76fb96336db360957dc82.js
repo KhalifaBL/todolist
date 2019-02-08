9a78726108995c629f79c9f6440b2c50a2d236ca
Khalifa <khalifalazreg@gmail.com>
2019-02-08 15:17:41 -0600

    Updates

---
+++ precache-manifest.db34b7e4d0c76fb96336db360957dc82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/todolist/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/todolist/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/todolist/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/todolist/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "f22540dbae59131f6235",
+    "url": "/todolist/static/js/main.f22540db.chunk.js"
+  },
+  {
+    "revision": "1ba169c509bbc032ea60",
+    "url": "/todolist/static/js/1.1ba169c5.chunk.js"
+  },
+  {
+    "revision": "f22540dbae59131f6235",
+    "url": "/todolist/static/css/main.82b5db5d.chunk.css"
+  },
+  {
+    "revision": "1ba169c509bbc032ea60",
+    "url": "/todolist/static/css/1.17634063.chunk.css"
+  },
+  {
+    "revision": "c91b0aff2f1a2636d0ad4323834d1fd2",
+    "url": "/todolist/index.html"
+  }
+];
